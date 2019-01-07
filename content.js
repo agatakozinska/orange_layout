7d91d960fd1835b76792a4fa257613e62af33232
Agata <kozinska.agata@gmail.com>
2019-01-07 22:41:37 +0100

    Add content and variables

--- content.js
+++ content.js
@@ -1,5 +1,36 @@
 let variables = {
-
+  nav_link_1: 'design',
+  nav_link_2: 'projects',
+  nav_link_3: 'about me',
+  nav_link_4: 'contact',
+  header_title: 'creative soul',
+  header_subtitle: 'meet me and my works<br>designer & content strategist from Warsaw',
+  intro_title: 'content & design strategy',
+  intro_text: 'Fashion axe austin DIY wolf 3 wolf moon, direct trade YOLO tote bag paleo blog. Meggings cloud bread cray, 3 wolf moon before they sold out literally normcore retro ennui fam umami letterpress actually distillery vinyl. Wayfarers slow-carb umami VHS. Unicorn activated charcoal fanny pack tbh subway tile knausgaard PBR&B.',
+  skills_title: 'the skillset',
+  skill_1: 'design',
+  skill_2: 'creative strategy',
+  skill_3: 'brainstorming session',
+  skill_4: 'planning',
+  skill_5: 'prototyping',
+  skills_note: 'read more on linkedin',
+  photo_caption: 'photo by Amy Ferris / Usplash',
+  project_title: 'projects',
+  project_content: 'Fashion axe austin DIY wolf 3 wolf moon, direct trade YOLO tote bag paleo blog.',
+  project_photo_heading_1: 'the vintage house',
+  project_photo_heading_2: 'colors of interior',
+  project_heading_1: 'the greenery',
+  project_heading_2: 'another project',
+  project_heading_3: 'and one more',
+  aboutme_title: 'for more updates follow me on',
+  aboutme_title_highlighed: 'instagram',
+  aboutme_note: '@thisismyinstagram',
+  contact_title: 'let\'s stay in touch',
+  contact_text: 'write me at cotact@theawwwesomes.org or catch me on social media',
+  footer_content: 'codenovember challenge organised by',
+  footer_organizer: 'The Awwwesomes',
+  footer_credit: 'photos are taken from Unsplash',
+  footer_data: 'Warsaw, 2018'
 }
 
 module.exports = variables;
