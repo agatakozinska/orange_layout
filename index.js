37b6f1bfb583b64678ef0794964390e422a02fa3
Agata <kozinska.agata@gmail.com>
2019-01-11 23:47:04 +0100

    Add hamburger button and style for mobile menu

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ window.addEventListener('scroll', function() {
   const nav = document.querySelector('.header__navigation');
   const navHeight = nav.offsetHeight;
   const pageHeight = window.scrollY;
+  const headerTitle = document.querySelector('.header__container');
+  
   if (pageHeight > navHeight) {
-    nav.classList.add('fixed');
+    nav.classList.add('fixed');  
+    headerTitle.style.marginTop = navHeight + 'px';
   } else {
     nav.classList.remove('fixed');
+    headerTitle.style.marginTop = '0px';
   }
 });
