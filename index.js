cb412112086f87ed33b45a0badd7bbc26bcffcba
Agata <kozinska.agata@gmail.com>
2019-01-12 01:28:44 +0100

    Add scrolling behavior for mobile menu

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 import "./index.scss";
 
-window.addEventListener('scroll', function() {
-  const nav = document.querySelector('.header__navigation');
-  const navHeight = nav.offsetHeight;
-  const pageHeight = window.scrollY;
-  const headerTitle = document.querySelector('.header__container');
-  
-  if (pageHeight > navHeight) {
-    nav.classList.add('fixed');  
-    headerTitle.style.marginTop = navHeight + 'px';
-  } else {
-    nav.classList.remove('fixed');
-    headerTitle.style.marginTop = '0px';
-  }
+const nav = document.querySelector('.header__navigation');
+
+if (window.screen.width >= 768) {
+  window.addEventListener('scroll', function() {
+    const navHeight = nav.offsetHeight;
+    const pageHeight = window.scrollY;
+    const headerTitle = document.querySelector('.header__container');
+    
+    if (pageHeight > navHeight) {
+      nav.classList.add('fixed');  
+      headerTitle.style.marginTop = navHeight + 'px';
+    } else {
+      nav.classList.remove('fixed');
+      headerTitle.style.marginTop = '0px';
+    }
+  });
+}
+
+const button = document.querySelector('.hamburger__button');
+const navVisible = 'header__navigation--visible';
+
+button.addEventListener('click', event => { 
+  nav.classList.toggle(navVisible);
+  stopScroll();
 });
+
+function stopScroll() {
+  if (nav.classList.contains(navVisible)) {
+    document.documentElement.classList.add("no-scroll");
+  } else {
+    document.documentElement.classList.remove("no-scroll");
+  }
+}
+
+const links = document.querySelectorAll('.navigation__list__item');
+const linksArray = [...links];
+
+for (let i = 0; i < linksArray.length; i++) {
+  linksArray[i].addEventListener("click", function() {
+    document.documentElement.classList.remove("no-scroll");
+    nav.classList.remove(navVisible);
+  });
+}
+
+
