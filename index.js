d95137ba64fa8dd606acedb17b00785ede39d1b0
Agata <kozinska.agata@gmail.com>
2019-02-23 22:05:30 +0100

    Fix issue with jumping content on scroll

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ import "./index.scss";
 
 const nav = document.querySelector('.header__navigation');
 
-if (window.screen.width >= 992) {
   window.addEventListener('scroll', function() {
     const navHeight = nav.offsetHeight;
     const pageHeight = window.scrollY;
-    const headerTitle = document.querySelector('.header__container');
-    
-    if (pageHeight > navHeight) {
+
+    if (window.screen.width >= 992 && (pageHeight > navHeight)) {
       nav.classList.add('fixed');  
-      headerTitle.style.marginTop = navHeight + 'px';
     } else {
       nav.classList.remove('fixed');
-      headerTitle.style.marginTop = '0px';
     }
-  });
-}
+});
 
 const button = document.querySelector('.hamburger__button');
 const navVisible = 'header__navigation--visible';
